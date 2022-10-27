b6acfdc86731dd1c9f4ab57f1d766845bed6ce66
rodrigolima6 <rodrigoolivallima6@gmail.com>
2022-10-27 11:58:02 +0100

    feat(Manager): Check channel string to process the sensor.

--- Manager.py
+++ Manager.py
@@ -8,9 +8,27 @@ class Manager(multiprocessing.Process):
         super().__init__()
         self.data = {}
 
+    def getOpenSignals(self):
+        for key in self.data["OpenSignals"].keys():
+            if key.startswith("ECG"):
+                print("ECG")
+                # Process ECG
+            if key.startswith("EDA"):
+                print("EDA")
+                # Process EDA
+            if key.startswith("RESP"):
+                print("RESP")
+                # Process EDA
+            if key.startswith("TEMP"):
+                print("SKT")
+                # Process EDA
+            if key.startswith("fNIRS"):
+                print("fNIRS")
+                # Process EDA
+
     def run(self):
 
-        sync = Sync(buffer_window=20)
+        sync = Sync(buffer_window=5)
         sync.start()
         sync.startAcquisition.value = 1
 
@@ -20,9 +38,13 @@ class Manager(multiprocessing.Process):
             elapsed_time = time.perf_counter() - start_time
             if sync.buffer_queue.qsize() > 0:
                 self.data = sync.buffer_queue.get()
-                print(self.data.keys())
+                print(self.data["OpenSignals"].keys())
 
-            if elapsed_time >= 30:
+                if "OpenSignals" in self.data.keys():
+                    self.getOpenSignals()
+                    # call function getOpenSignals.
+
+            if elapsed_time >= 10:
                 sync.startAcquisition.value = 0
                 plt.figure()
                 plt.plot(self.data["OpenSignals"]["RAW0"])
