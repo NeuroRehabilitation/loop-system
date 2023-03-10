b08a18cdf900a057b8a8692bf1a1adec76522572
rodrigolima6 <rodrigoolivallima6@gmail.com>
2023-03-10 11:56:08 +0000

    fix(Process_Features,Load,Training Model,Signals_Processing): Fix how data was organized in dictionaries.

--- Training Model.py
+++ Training Model.py
@@ -32,6 +32,7 @@ for root, dirs, files in os.walk(path):
 Opensignals_fs = 1000
 EEG_fs = 250
 resolution = 16
+sensors = ["ECG", "EDA", "RESP", "TEMP", "fnirs1", "fnirs2"]
 
 (
     events_diff,
@@ -45,9 +46,15 @@ resolution = 16
     data,
     valence,
     arousal,
-) = getEvents(users, "PsychoPy Markers", "PsychoPy Ratings")
-print(valence)
-print(arousal)
+) = getEvents(
+    users,
+    "OpenSignals",
+    "openvibeSignal",
+    "PsychoPyStream",
+    "PsychoPy Ratings",
+    sensors,
+)
+
 """EEG Processing"""
 
 EEG_filtered = filterEEG(data, EEG_fs)
@@ -58,7 +65,7 @@ features_EEG = getEEGBands(EEG_dict, EEG_fs)
 features_epochs_EEG = getEEGDict(features_EEG)
 
 """Biosignalsplux Processing"""
-Signals_epochs = getEpochs(data, onset_index, events_diff, Opensignals_fs)
+Signals_epochs = getSignalsEpochs(data, onset_index, events_diff, Opensignals_fs)
 Signals = getVideosDict(Signals_epochs, videos)
 features_signals = getFeatures(Signals, Opensignals_fs, resolution)
 features_epochs = getFeaturesEpochs(features_signals)
@@ -72,34 +79,34 @@ columns = dataframe.columns[: (len(dataframe.columns) - 2)]
 columns_EEG = dataframe_EEG.columns[: (len(dataframe_EEG.columns) - 2)]
 full_dataframe = pd.concat([dataframe_EEG[columns_EEG], dataframe], axis=1)
 full_columns = full_dataframe.columns[: (len(full_dataframe.columns) - 2)]
-
-"""Scaler"""
-scaler = StandardScaler()
-
-"""Input Data for Models"""
-X = np.array(full_dataframe[full_columns])
-Y = np.array(full_dataframe[["Category"]])
-
-"""Imputer for Nan"""
-imp = SimpleImputer(missing_values=np.nan, strategy="mean")
-imp.fit(X)
-X = imp.transform(X)
-
-"""Train Model"""
-
-model = ensemble.RandomForestClassifier(random_state=0)
-scaler = scaler.fit(X, Y.ravel())
-X = scaler.transform(X)
-rfe = feature_selection.RFE(model, step=1)
-rfe = rfe.fit(X, Y.ravel())
-X = rfe.transform(X)
-model.fit(X, Y.ravel())
-
-with open(path + "//" + "imp.pkl", "wb") as f:
-    pickle.dump(imp, f)
-with open(path + "//" + "scaler.pkl", "wb") as f:
-    pickle.dump(scaler, f)
-with open(path + "//" + "rfe.pkl", "wb") as f:
-    pickle.dump(rfe, f)
-with open(path + "//" + "model.pkl", "wb") as f:
-    pickle.dump(model, f)
+#
+# """Scaler"""
+# scaler = StandardScaler()
+#
+# """Input Data for Models"""
+# X = np.array(full_dataframe[full_columns])
+# Y = np.array(full_dataframe[["Category"]])
+#
+# """Imputer for Nan"""
+# imp = SimpleImputer(missing_values=np.nan, strategy="mean")
+# imp.fit(X)
+# X = imp.transform(X)
+#
+# """Train Model"""
+#
+# model = ensemble.RandomForestClassifier(random_state=0)
+# scaler = scaler.fit(X, Y.ravel())
+# X = scaler.transform(X)
+# rfe = feature_selection.RFE(model, step=1)
+# rfe = rfe.fit(X, Y.ravel())
+# X = rfe.transform(X)
+# model.fit(X, Y.ravel())
+#
+# with open(path + "//" + "imp.pkl", "wb") as f:
+#     pickle.dump(imp, f)
+# with open(path + "//" + "scaler.pkl", "wb") as f:
+#     pickle.dump(scaler, f)
+# with open(path + "//" + "rfe.pkl", "wb") as f:
+#     pickle.dump(rfe, f)
+# with open(path + "//" + "model.pkl", "wb") as f:
+#     pickle.dump(model, f)
