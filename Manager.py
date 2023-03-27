eace4abd62bd21ec9e2fcf23d7159db1e9eb7948
rodrigolima6 <rodrigoolivallima6@gmail.com>
2023-03-27 15:12:44 +0100

    fix(Manager): fix algorithms to get features.

--- Manager.py
+++ Manager.py
@@ -1,43 +1,67 @@
 from Sync import *
 from Process import *
-import time
 
 
 class Manager:
     def run(self):
+
+        folder = os.getcwd() + "\\Training Models\\"
+        participant = "P2"
+        path = folder + participant
+
         # Instantiate object from class Sync and Processing
-        sync = Sync(buffer_window=40)
+        sync = Sync(buffer_window=30)
         process = Processing()
 
         # Start process Sync and put flag startAcquisition as True
         sync.start()
         sync.startAcquisition.value = 1
 
+        imp, scaler, rfe, model = process.loadModels(path)
+        imp_arousal, scaler_arousal, rfe_arousal, model_arousal = process.loadModels(
+            path, target="_arousal"
+        )
+        imp_valence, scaler_valence, rfe_valence, model_valence = process.loadModels(
+            path, target="_valence"
+        )
+
+        EEGbaseline = pd.read_csv(path + "\\EEGbaseline.csv", sep=";", index_col=0)
+        baseline = pd.read_csv(path + "\\baseline.csv", sep=";", index_col=0)
+
+        dataframe_baseline = pd.concat([EEGbaseline, baseline], axis=1)
         # Get streams information
         process.info = sync.info_queue.get()
 
         # While is acquiring data
-        while True:
+        while bool(sync.startAcquisition.value):
             if sync.markers_queue.qsize() > 0:
                 video, marker = sync.markers_queue.get()
                 print("Video = " + str(video))
-                # if video == "end":
-                #     sync.startAcquisition.value = 0
+                if video == "end":
+                    print("end")
+                    sync.startAcquisition.value = 0
                 if video == "start":
-                    sync.videoStarted.value = 1
+                    sync.startBuffer.value = 1
             if sync.arousal_queue.qsize() > 0:
-                arousal = sync.arousal_queue.get()
-                print("Arousal = " + str(arousal))
+                true_arousal = sync.arousal_queue.get()
+                arousal = process.predict(
+                    imp_arousal, scaler_arousal, rfe_arousal, model_arousal
+                )
+                print("Arousal Prediction = " + str(arousal))
             if sync.valence_queue.qsize() > 0:
-                valence = sync.valence_queue.get()
-                print("Valence = " + str(valence))
+                true_valence = sync.valence_queue.get()
+                print("Valence = " + str(true_valence))
+                valence = process.predict(
+                    imp_valence, scaler_valence, rfe_valence, model_valence
+                )
+                print("Valence Prediction = " + str(valence))
             # If there is data in the buffer queue from Sync, send to Process.
             if sync.buffer_queue.qsize() > 0:
                 process.data = sync.buffer_queue.get()
                 process.features = process.processData()
+                process.features -= dataframe_baseline
                 print(process.features)
-
-            # Set end of acquisition in seconds and put flag startAcquisition as False.
+                category = process.predict(imp, scaler, rfe, model)
 
         sync.terminate()
         sync.join()
