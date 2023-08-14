056f57b2523860d7f43729dfbac12244b6651eb4
rodrigolima6 <rodrigoolivallima6@gmail.com>
2023-08-14 10:51:29 +0100

    fix(Exceptions): Handle exceptions on runtime.

--- Process.py
+++ Process.py
@@ -13,83 +13,142 @@ class Processing:
             if stream["Name"] == "OpenSignals":
                 name = stream["Name"]
                 fs = stream["Sampling Rate"]
-                for key in self.data[name].keys():
-                    if key.startswith("ECG"):
-                        HRV_Dataframe = Process_HRV(self.data[name][key], fs, 16)
-                        self.features = pd.concat(
-                            [self.features, HRV_Dataframe], axis=1
-                        )
+                try:
+                    for key in self.data[name].keys():
+                        if key.startswith("ECG"):
+                            try:
+                                HRV_Dataframe = Process_HRV(
+                                    self.data[name][key], fs, 16
+                                )
+                                self.features = pd.concat(
+                                    [self.features, HRV_Dataframe], axis=1
+                                )
+                            except Exception as e:
+                                print(e)
+                                print("Error on HRV Dataframe!")
 
-                    elif key.startswith("EDA"):
-                        EDA_Dataframe = Process_EDA(self.data[name][key], fs, 16)
-                        self.features = pd.concat(
-                            [self.features, EDA_Dataframe], axis=1
-                        )
+                        elif key.startswith("EDA"):
+                            try:
+                                EDA_Dataframe = Process_EDA(
+                                    self.data[name][key], fs, 16
+                                )
+                                self.features = pd.concat(
+                                    [self.features, EDA_Dataframe], axis=1
+                                )
+                            except Exception as e:
+                                print(e)
+                                print("Error on EDA Dataframe!")
 
-                    elif key.startswith("RESP"):
-                        RESP_Dataframe = Process_RESP(self.data[name][key], fs, 16)
-                        self.features = pd.concat(
-                            [self.features, RESP_Dataframe], axis=1
-                        )
-
-        return self.features
+                        elif key.startswith("RESP"):
+                            try:
+                                RESP_Dataframe = Process_RESP(
+                                    self.data[name][key], fs, 16
+                                )
+                                self.features = pd.concat(
+                                    [self.features, RESP_Dataframe], axis=1
+                                )
+                            except Exception as e:
+                                print(e)
+                except Exception as e:
+                    print(e)
+                finally:
+                    return self.features
 
     def getOpenvibe(self):
         for stream in self.info:
             if stream["Name"] == "openvibeSignal":
-                fs = stream["Sampling Rate"]
-                # EEG_filtered = filterEEG(self.data["openvibeSignal"], fs)
-                # print(EEG_filtered)
-                EEG_dict = {}
-                for i, key in enumerate(self.data["openvibeSignal"]):
-                    if i > 0 and i < 33:
-                        channel = "EEG_" + str(i)
-                        #         sensor = EEG(self.data["openvibeSignal"][key], fs, 16)
-                        EEG_dict[channel] = EEG.frequencyAnalysis(
-                            EEG.filterData(
-                                EEG.ICA(self.data["openvibeSignal"][key]), fs
-                            ),
-                            fs,
+                try:
+                    fs = stream["Sampling Rate"]
+                    # EEG_filtered = filterEEG(self.data["openvibeSignal"], fs)
+                    # print(EEG_filtered)
+                    EEG_dict = {}
+                    for i, key in enumerate(self.data["openvibeSignal"]):
+                        try:
+                            if i > 0 and i < 33:
+                                channel = "EEG_" + str(i)
+                                #         sensor = EEG(self.data["openvibeSignal"][key], fs, 16)
+                                EEG_dict[channel] = EEG.frequencyAnalysis(
+                                    EEG.filterData(
+                                        EEG.ICA(self.data["openvibeSignal"][key]), fs
+                                    ),
+                                    fs,
+                                )
+                        except Exception as e:
+                            print(e)
+                    columns, temp_list = [], []
+                    for channel in EEG_dict.keys():
+                        for band in EEG_dict[channel].keys():
+                            columns.append(channel + "_" + band)
+                            temp_list.append(EEG_dict[channel][band])
+                    EEG_Dataframe = pd.DataFrame(columns=columns)
+                    EEG_Dataframe.loc[0] = temp_list
+                    try:
+                        self.features = pd.concat(
+                            [EEG_Dataframe, self.features], axis=1
                         )
-                columns, temp_list = [], []
-                for channel in EEG_dict.keys():
-                    for band in EEG_dict[channel].keys():
-                        columns.append(channel + "_" + band)
-                        temp_list.append(EEG_dict[channel][band])
-                EEG_Dataframe = pd.DataFrame(columns=columns)
-                EEG_Dataframe.loc[0] = temp_list
-                self.features = pd.concat([EEG_Dataframe, self.features], axis=1)
+                    except Exception as e:
+                        print(e)
+                except Exception as e:
+                    print(e)
+                finally:
+                    return self.features
                 # print(EEG_Dataframe)
                 # print(self.features)
-        return self.features
 
     def processData(self):
         self.features = pd.DataFrame()
-
-        if "OpenSignals" in self.data.keys():
-            self.features = self.getOpenSignals()
-        if "openvibeSignal" in self.data.keys():
-            self.features = self.getOpenvibe()
+        try:
+            if "OpenSignals" in self.data.keys():
+                self.features = self.getOpenSignals()
+        except Exception as e:
+            print(e)
+            print("OpenSignals Error!")
+        try:
+            if "openvibeSignal" in self.data.keys():
+                self.features = self.getOpenvibe()
+        except Exception as e:
+            print(e)
+            print("Openvibe Error!")
 
         return self.features
 
     def loadModels(self, path: str, target=""):
-        imp = pickle.load(open(path + "\\imp" + target + ".pkl", "rb"))
-        scaler = pickle.load(open(path + "\\scaler" + target + ".pkl", "rb"))
-        rfe = pickle.load(open(path + "\\rfe" + target + ".pkl", "rb"))
-        model = pickle.load(open(path + "\\model" + target + ".pkl", "rb"))
+        try:
+            imp = pickle.load(open(path + "\\imp" + target + ".pkl", "rb"))
+        except Exception as e:
+            print(e)
+            print("Error on loading Imputer file!")
+        try:
+            scaler = pickle.load(open(path + "\\scaler" + target + ".pkl", "rb"))
+        except Exception as e:
+            print(e)
+            print("Error on loading Scaler file!")
+        try:
+            rfe = pickle.load(open(path + "\\rfe" + target + ".pkl", "rb"))
+        except Exception as e:
+            print(e)
+            print("Error on loading RFE file!")
+        try:
+            model = pickle.load(open(path + "\\model" + target + ".pkl", "rb"))
+        except Exception as e:
+            print(e)
+            print("Error on loading Model file!")
 
         return imp, scaler, rfe, model
 
     def predict(self, imp, scaler, rfe, model):
-        X = np.array(self.features)
-        if len(X) > 0:
-            X = imp.transform(X)
-            X = scaler.transform(X)
-            X = rfe.transform(X)
+        try:
+            X = np.array(self.features)
+            if len(X) > 0:
+                X = imp.transform(X)
+                X = scaler.transform(X)
+                X = rfe.transform(X)
 
-            prediction = model.predict(X)
+                prediction = model.predict(X)
 
-            return prediction
-        else:
-            pass
+                return prediction
+            else:
+                print("X has no data!")
+        except Exception as e:
+            print(e)
+            print("No prediction")
