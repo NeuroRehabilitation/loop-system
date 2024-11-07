f8a41abb7c9a6fbd179d721fb79d7a8a3cae602e
rodrigolima6 <rodrigoolivallima6@gmail.com>
2024-11-07 11:47:25 +0000

    fix(Signals_Processing, Training Model): update functions to obtain markers and ratings.

--- Signals_Processing.py
+++ Signals_Processing.py
@@ -1,4 +1,101 @@
+import pyxdf
+from colorama import Fore
+from tqdm import tqdm
+
+from Epochs import *
+from Load import *
 from Process_Features import *
+
+
+@staticmethod
+def getSignals(data,
+               openSignals_stream_name: str,
+               markers_stream_name: str,
+               ratings_stream_name: str,
+               sensors: list, ) -> dict:
+    signals = {}
+
+    for condition in data:
+        signals[condition] = Load_Data(data[condition], openSignals_stream_name, markers_stream_name,
+                                       ratings_stream_name, sensors)
+
+    return signals
+
+
+@staticmethod
+def getEvents(signals) -> (dict, dict):
+    epochs_markers, ratings = {}, {}
+
+    for condition in signals:
+        onset, offset, marker = getMarkers(signals[condition]["Markers"], signals[condition]["Markers Timestamps"])
+        onset_index, offset_index = getMarkersIndex(onset, offset, signals[condition]["Signals"]["Time"])
+        events_diff = CalculateEventsDiff(onset, offset)
+        epochs_markers[condition] = {"Onset": onset, "Onset_index": onset_index,
+                                     "Offset": offset, "Offset_index": offset_index,
+                                     "EventsDiff": events_diff,
+                                     "Marker": marker}
+
+        arousal_index = getRatingsIndex(signals[condition]["Ratings"]["Arousal Timestamps"],
+                                        signals[condition]["Signals"]["Time"])
+        valence_index = getRatingsIndex(signals[condition]["Ratings"]["Valence Timestamps"],
+                                        signals[condition]["Signals"]["Time"])
+        ratings[condition] = {"Arousal": signals[condition]["Ratings"]["Arousal"],
+                              "Arousal Timestamps": signals[condition]["Ratings"]["Arousal Timestamps"],
+                              "Arousal_index": arousal_index,
+                              "Valence": signals[condition]["Ratings"]["Valence"],
+                              "Valence Timestamps": signals[condition]["Ratings"]["Valence Timestamps"],
+                              "Valence_index": valence_index}
+
+    return epochs_markers, ratings
+
+
+@staticmethod
+def Run_files(fname):
+    data, header = pyxdf.load_xdf(fname)
+
+    return data
+
+
+@staticmethod
+def change_key(dictionary, key_to_remove, new_key):
+    dictionary[new_key] = dictionary.pop(key_to_remove)
+
+
+@staticmethod
+def Load_Data(
+        data,
+        openSignals_stream_name: str,
+        markers_stream_name: str,
+        ratings_stream_name: str,
+        sensors: list,
+) -> dict:
+    marker, timestamps = Load_PsychopyMarkers(data, markers_stream_name)
+    opensignals_data, fs = Load_Opensignals(data, openSignals_stream_name)
+    ratings = Load_Ratings(data, ratings_stream_name)
+
+    change_key(opensignals_data, "time_Opensignals", "Time"), change_key(opensignals_data, "CH1", sensors[0]),
+    change_key(opensignals_data, "CH2", sensors[1]), change_key(opensignals_data, "CH3", sensors[2])
+
+    Signals = pd.DataFrame(data=opensignals_data)
+
+    data = {
+        "Signals": Signals,
+        "Markers": marker,
+        "Markers Timestamps": timestamps,
+        "Ratings": ratings
+    }
+
+    return data
+
+
+@staticmethod
+def getDataframe(dataframe, fs, resolution):
+    HRV_Dataframe = Process_HRV(dataframe["ECG"], fs, resolution)
+    RESP_Dataframe = Process_RESP(dataframe["RESP"], fs, resolution)
+    EDA_Dataframe = Process_EDA(dataframe["EDA"], fs, resolution)
+    Dataframe = (HRV_Dataframe.join(EDA_Dataframe)).join(RESP_Dataframe)
+
+    return Dataframe
 
 
 @staticmethod
@@ -40,19 +137,111 @@ def getEpochs(data: dict, onset_index: dict, events_diff: dict, fs: int) -> dict
     return epochs
 
 
-def getSignalsEpochs(data: dict, onset_index: dict, events_diff: dict, fs: int) -> dict:
-    epochs = {}
+@staticmethod
+def getSignalsEpochs(signals: dict, epochs_markers: dict, ratings: dict, window: int, fs: int) -> dict:
+    epochs_data = {}
 
-    for users in data.keys():
-        epochs[users] = nk.epochs_create(
-            data[users]["Signals"],
-            events=onset_index[users],
-            sampling_rate=fs,
-            epochs_start=0,
-            epochs_end=events_diff[users],
-        )
+    for condition in epochs_markers:
+        events = np.arange(epochs_markers[condition]["Onset_index"][0],
+                           epochs_markers[condition]["Offset_index"][-1] - window * fs, fs)
 
-    return epochs
+        stroop_markers = [0]
+
+        for timestamp_index in epochs_markers[condition]["Onset_index"]:
+            stroop_markers.append(
+                len(np.where(timestamp_index >= events)[0]))
+
+        count = np.diff(stroop_markers)
+        markers = [value for count, value in zip(
+            count, epochs_markers[condition]["Marker"]) for _ in range(count)]
+
+        arousal_values = markers
+        valence_values = markers
+
+        epochs_signals = nk.epochs_create(pd.DataFrame.from_dict(signals[condition]["Signals"]),
+                                          events=events,
+                                          sampling_rate=fs,
+                                          epochs_start=0,
+                                          epochs_end=window,
+                                          )
+
+        epochs_data[condition] = {"Signals": epochs_signals, "Arousal": arousal_values, "Valence": valence_values}
+
+    return epochs_data
+
+
+@staticmethod
+def getFeatures(epochs_data: dict, fs: int, resolution: int) -> dict:
+    # ANSI escape code for blue
+    BLUE = "\033[94m"
+
+    features = {}
+
+    for condition in tqdm(epochs_data):
+        features[condition] = {}
+        for epoch in tqdm(epochs_data[condition]["Signals"],
+                          desc=Fore.BLUE + f"Extracting feature for {condition}" + Fore.BLUE,
+                          colour="cyan",
+                          bar_format=BLUE + "{l_bar}" + BLUE + "{bar}" + BLUE + "| " + BLUE + "{n_fmt}" + BLUE + "/" + BLUE + "{total_fmt} [" + BLUE + "{elapsed}" + "<" + BLUE + "{remaining}" + ", " + "{rate_fmt}""]",
+                          leave=False):
+            features[condition][epoch] = getDataframe(epochs_data[condition]["Signals"][epoch], fs, resolution)
+
+    tqdm().set_description("All Conditions Processed")
+
+    return features
+
+
+@staticmethod
+def getFeaturesEpochs(data: dict) -> tuple:
+    features_epochs, baseline_dict = {}, {}
+    df_baseline = pd.DataFrame()
+    for user in data.keys():
+        temp_dict = {}
+        for video in data[user].keys():
+            df_test = pd.DataFrame()
+
+            if video != "baseline":
+                for feature in data[user][video].columns:
+                    df_test[feature] = (
+                            data[user][video][feature] - data[user]["baseline"][feature]
+                    )
+                temp_dict[video] = df_test
+            elif video == "baseline":
+                for feature in data[user][video].columns:
+                    df_baseline[feature] = data[user]["baseline"][feature]
+
+        features_epochs[user] = temp_dict
+        baseline_dict[user] = df_baseline
+
+    return features_epochs, baseline_dict
+
+
+@staticmethod
+def getSignalsDataframe(features: dict, epochs_data: dict):
+    BLUE = "\033[94m"
+
+    dataframe = pd.DataFrame(columns=features["Baseline"]["1"].columns)
+    users, conditions, arousal, valence = [], [], [], []
+
+    for condition in tqdm(features):
+        arousal.extend(epochs_data[condition]["Arousal"])
+        valence.extend(epochs_data[condition]["Valence"])
+
+        for epoch in tqdm(features[condition],
+                          desc=Fore.BLUE + f"Concatenating Features in Dataframe for {condition}" + Fore.BLUE,
+                          colour="cyan",
+                          bar_format=BLUE + "{l_bar}" + BLUE + "{bar}" + BLUE + "| " + BLUE + "{n_fmt}" + BLUE + "/" + BLUE + "{total_fmt} [" + BLUE + "{elapsed}" + "<" + BLUE + "{remaining}" + ", " + "{rate_fmt}""]",
+                          leave=False):
+            conditions.append(condition + "_" + epoch)
+            dataframe = pd.concat([dataframe, features[condition][epoch]], ignore_index=True)
+
+    # dataframe["Arousal"] = arousal
+    # dataframe["Valence"] = valence
+    dataframe["Condition"] = conditions
+    dataframe = dataframe.rename(
+        columns={"AVG": "AVG_RSP_Rate", "Maximum": "Max_RSP_Rate", "Minimum": "Min_RSP_Rate", "STD": "STD_RSP_Rate"})
+
+    return dataframe
 
 
 @staticmethod
@@ -100,8 +289,8 @@ def getEEGDict(features_EEG: dict) -> tuple:
                     band_dict = {}
                     for feature in features_EEG[user][video][channel].keys():
                         band_dict[feature] = (
-                            features_EEG[user][video][channel][feature]
-                            - features_EEG[user]["baseline"][channel][feature]
+                                features_EEG[user][video][channel][feature]
+                                - features_EEG[user]["baseline"][channel][feature]
                         )
                     channel_dict[channel] = band_dict
                 temp_dict[video] = channel_dict
@@ -118,44 +307,6 @@ def getEEGDict(features_EEG: dict) -> tuple:
         features_baseline_EEG[user] = channel_dict
 
     return features_epochs_EEG, features_baseline_EEG
-
-
-@staticmethod
-def getFeatures(data: dict, fs: int, resolution: int) -> dict:
-    features_signals = {}
-
-    for users in data.keys():
-        temp_dict = {}
-        for key in data[users].keys():
-            temp_dict[key] = getDataframe(data[users][key], fs, resolution)
-        features_signals[users] = temp_dict
-
-    return features_signals
-
-
-@staticmethod
-def getFeaturesEpochs(data: dict) -> tuple:
-    features_epochs, baseline_dict = {}, {}
-    df_baseline = pd.DataFrame()
-    for user in data.keys():
-        temp_dict = {}
-        for video in data[user].keys():
-            df_test = pd.DataFrame()
-
-            if video != "baseline":
-                for feature in data[user][video].columns:
-                    df_test[feature] = (
-                        data[user][video][feature] - data[user]["baseline"][feature]
-                    )
-                temp_dict[video] = df_test
-            elif video == "baseline":
-                for feature in data[user][video].columns:
-                    df_baseline[feature] = data[user]["baseline"][feature]
-
-        features_epochs[user] = temp_dict
-        baseline_dict[user] = df_baseline
-
-    return features_epochs, baseline_dict
 
 
 @staticmethod
@@ -199,23 +350,3 @@ def getEEGDataframe(features_epochs_EEG: dict, features_baseline_EEG: dict):
     df_EEG["Video"] = video
 
     return df_EEG, baseline_df
-
-
-@staticmethod
-def getSignalsDataframe(features_epochs: dict):
-    category, videos = [], []
-
-    for key in features_epochs.keys():
-        user = key
-    video = list(features_epochs[user].keys())
-    df = pd.DataFrame(columns=features_epochs[user][video[0]].columns)
-    for users in features_epochs.keys():
-        for epoch in features_epochs[users].keys():
-            if epoch != "baseline":
-                df = pd.concat([df, features_epochs[users][epoch]], ignore_index=True)
-                category.append(epoch.split("/")[1])
-                videos.append(epoch.split("/")[2])
-    df["Category"] = category
-    df["Video"] = videos
-
-    return df
--- Training Model.py
+++ Training Model.py
@@ -1,15 +1,5 @@
 import warnings
 
-import joblib
-from sklearn import ensemble, feature_selection, svm, naive_bayes, neighbors
-from sklearn.ensemble import VotingClassifier
-from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-from sklearn.neural_network import MLPClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-
 from Signals_Processing import *
 
 warnings.filterwarnings("ignore")
@@ -17,155 +7,168 @@ warnings.filterwarnings("ignore")
 """Load Data from Folder"""
 
 folder = os.getcwd() + "\\Training Models\\"
-participant = "P0"
+participant = "P3"
 path = folder + participant
 
-os.chdir(path)
-(
-    users,
-    Signals_epochs,
-    Signals,
-    features_signals,
-    features_epochs,
-) = ({}, {}, {}, {}, {})
-
-for root, dirs, files in os.walk(path):
-    for fname in files:
-        if fname.endswith(".xdf"):
-            users[fname] = Run_files(fname)
-
-Opensignals_fs = 100
+data = {}
+fs = 100
 resolution = 16
 sensors = ["ECG", "EDA", "RESP"]
 
-(
-    events_diff,
-    videos,
-    onset,
-    offset,
-    onset_index,
-    offset_index,
-    data,
-    valence,
-    arousal,
-) = getEvents(
-    users,
-    "OpenSignals",
-    "PsychoPy Markers",
-    sensors
-)
-
-"""Biosignalsplux Processing"""
-
-Signals_epochs = getSignalsEpochs(data, onset_index, events_diff, Opensignals_fs)
-Signals = getVideosDict(Signals_epochs, videos)
-features_signals = getFeatures(Signals, Opensignals_fs, resolution)
-features_epochs, df_baseline = getFeaturesEpochs(features_signals)
-df_baseline[participant].to_csv(path + "\\baseline.csv", sep=";")
-
-"""Dataframe"""
-dataframe = getSignalsDataframe(features_epochs)
-dataframe["Valence Level"] = valence[participant]
-dataframe["Arousal Level"] = arousal[participant]
-columns = dataframe.columns[: (len(dataframe.columns) - 4)]
-
-"""Scaler"""
-scaler = StandardScaler(),
-
-"""Input Data for Models"""
-X = np.array(dataframe[columns])
-Y = np.array(dataframe[["Category"]])
-
-"""Models Parameters for GridSearch"""
-
-param_grids = {
-    "KNN": {
-        'model': [neighbors.KNeighborsClassifier()],
-        'model__n_neighbors': list(range(1, 5)),
-        'model__weights': ['uniform', 'distance'],
-        'model__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
-    },
-    "SVM": {
-        'model': [svm.SVC(probability=True)],
-        'model__C': [0.1, 1, 10],
-        'model__kernel': ['linear', 'rbf'],
-        'model__gamma': ["scale", "auto"],
-        'model__random_state': list(range(0, 50, 2))
-    },
-    "Bayes": {
-        'model': [naive_bayes.GaussianNB()]
-    },  # No parameters for GaussianNB
-
-    "Logistic Regression": {
-        'model': [LogisticRegression()],
-        'model__C': [0.1, 1, 10],
-        'model__solver': ['lbfgs', 'newton-cg', 'sag', 'saga'],
-        'model__multi_class': ["multinomial"],
-        'model__random_state': list(range(0, 50, 5))
-    },
-    "Random Forest": {
-        'model': [ensemble.RandomForestClassifier()],
-        'model__n_estimators': [50, 100, 150],
-        'model__criterion': ["gini", "entropy", "log_loss"],
-        'model__random_state': list(range(0, 50, 5))
-    },
-    "NN": {
-        'model': [MLPClassifier()],
-        'model__activation': ['identity', 'logistic', 'tanh', 'relu'],
-        'model__solver': ['lbfgs', 'sgd', 'adam'],
-        'model__random_state': list(range(0, 50, 5))
-    }
-}
-
-"""Train Model using GridSearchCV"""
-
-best_models = {}
-
-# Loop through each model and parameter grid
-for model_name, param_grid in param_grids.items():
-    # Create the pipeline
-    pipeline = Pipeline([
-        ('imputer', SimpleImputer(missing_values=np.nan, strategy="mean")),
-        ('scaler', StandardScaler()),
-        ('feature_selection', feature_selection.RFE(ensemble.RandomForestClassifier(random_state=42), step=1)),
-        ('model', param_grid['model'][0])  # Placeholder model to set pipeline structure
-    ])
-
-    # Grid search for hyperparameter tuning
-    if param_grid:
-        grid_search = GridSearchCV(
-            pipeline, param_grid, cv=5, n_jobs=-1, scoring='accuracy')
-        grid_search.fit(X, Y.ravel())
-
-        # Store the best model and score
-        best_models[model_name] = {
-            'best_estimator': grid_search.best_estimator_,
-            'best_score': grid_search.best_score_
-        }
-        print(f"Best cross-validation accuracy for {model_name}: {grid_search.best_score_:.4f}")
-    else:
-        # Fit the pipeline without grid search if no parameters
-        pipeline.fit(X, Y.ravel())
-        best_models[model_name] = {
-            'best_estimator': pipeline,
-            'best_score': pipeline.score(X, Y.ravel())  # Evaluate the score directly
-        }
-        print(f"No hyperparameters to tune for {model_name}. Model fitted directly.")
-
-# Sort the models by their best score in descending order
-sorted_models = sorted(best_models.items(), key=lambda item: item[1]['best_score'], reverse=True)
-
-# Select the top two models
-best_two_models = sorted_models[:2]
-
-estimators = [(model_name, info['best_estimator']) for model_name, info in best_two_models]
-
-# Create a VotingClassifier with the best two models
-voting_clf = VotingClassifier(estimators=estimators, voting='soft')  # You can also use 'hard' voting
-voting_clf.fit(X, Y.ravel())
-
 try:
-    joblib.dump(voting_clf, f"{path}/model.pkl")
-    print(f'VotingClassifier saved successfully to {path}')
+    if os.path.isdir(path):
+        print(f"Loading data from {path}.")
+
+        xdf_files = [file for file in os.listdir(path) if file.endswith('.xdf')]
+
+        if not xdf_files:
+            print("No .xdf files found.")
+        else:
+            for file in xdf_files:
+                file_path = os.path.join(path, file)
+
+                condition = file.split("_")[1].split(".")[0]
+
+                data[condition] = Run_files(file_path)
+
+except FileNotFoundError:
+    print(f"The directory {path} does not exist.")
 except Exception as e:
-    print(f'Error saving the model: {e}')
+    print(f"An error occurred: {e}")
+
+"""Signals Processing"""
+signals = getSignals(data, "OpenSignals", "PsychoPy Markers", "PsychoPy Ratings", sensors=sensors)
+epochs_markers, ratings = getEvents(signals)
+
+"""Baseline Dataframe"""
+baseline_signals = nk.epochs_create(
+    pd.DataFrame.from_dict(signals["Baseline"]["Signals"]),
+    events=epochs_markers["Baseline"]["Onset_index"],
+    sampling_rate=fs,
+    epochs_start=0,
+    epochs_end=epochs_markers["Baseline"]["EventsDiff"],
+)
+df_baseline = getDataframe(baseline_signals["1"], fs, resolution)
+try:
+    df_baseline.to_csv(path + "\\baseline.csv", sep=";")
+    print(f"Baseline Dataframe saved to {path}.")
+except Exception as e:
+    print(f"An error occurred saving the Baseline Dataframe: {e}")
+
+"""Create epochs from signals"""
+epochs_data = getSignalsEpochs(signals, epochs_markers, ratings, window=60, fs=fs)
+print("Epochs Created Successfully.")
+
+"""Extract features from Epochs"""
+features = getFeatures(epochs_data, fs=fs, resolution=resolution)
+
+"""Features Dataframe"""
+dataframe = getSignalsDataframe(features, epochs_data)
+
+# features_signals = getFeatures(Signals, Opensignals_fs, resolution)
+# features_epochs, df_baseline = getFeaturesEpochs(features_signals)
+# df_baseline[participant].to_csv(path + "\\baseline.csv", sep=";")
+#
+# """Dataframe"""
+# dataframe = getSignalsDataframe(features_epochs)
+# dataframe["Valence Level"] = valence[participant]
+# dataframe["Arousal Level"] = arousal[participant]
+# columns = dataframe.columns[: (len(dataframe.columns) - 4)]
+#
+# """Input Data for Models"""
+# X = np.array(dataframe[columns])
+# Y = np.array(dataframe[["Category"]])
+#
+# """Models Parameters for GridSearch"""
+#
+# param_grids = {
+#     "KNN": {
+#         'model': [neighbors.KNeighborsClassifier()],
+#         'model__n_neighbors': list(range(1, 5)),
+#         'model__weights': ['uniform', 'distance'],
+#         'model__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+#     },
+#     "SVM": {
+#         'model': [svm.SVC(probability=True)],
+#         'model__C': [0.1, 1, 10],
+#         'model__kernel': ['linear', 'rbf'],
+#         'model__gamma': ["scale", "auto"],
+#         'model__random_state': list(range(0, 50, 2))
+#     },
+#     "Bayes": {
+#         'model': [naive_bayes.GaussianNB()]
+#     },  # No parameters for GaussianNB
+#
+#     "Logistic Regression": {
+#         'model': [LogisticRegression()],
+#         'model__C': [0.1, 1, 10],
+#         'model__solver': ['lbfgs', 'newton-cg', 'sag', 'saga'],
+#         'model__multi_class': ["multinomial"],
+#         'model__random_state': list(range(0, 50, 5))
+#     },
+#     "Random Forest": {
+#         'model': [ensemble.RandomForestClassifier()],
+#         'model__n_estimators': [50, 100, 150],
+#         'model__criterion': ["gini", "entropy", "log_loss"],
+#         'model__random_state': list(range(0, 50, 5))
+#     },
+#     "NN": {
+#         'model': [MLPClassifier()],
+#         'model__activation': ['identity', 'logistic', 'tanh', 'relu'],
+#         'model__solver': ['lbfgs', 'sgd', 'adam'],
+#         'model__random_state': list(range(0, 50, 5))
+#     }
+# }
+#
+# """Train Model using GridSearchCV"""
+#
+# best_models = {}
+#
+# # Loop through each model and parameter grid
+# for model_name, param_grid in param_grids.items():
+#     # Create the pipeline
+#     pipeline = Pipeline([
+#         ('imputer', SimpleImputer(missing_values=np.nan, strategy="mean")),
+#         ('scaler', StandardScaler()),
+#         ('feature_selection', feature_selection.RFE(ensemble.RandomForestClassifier(random_state=42), step=1)),
+#         ('model', param_grid['model'][0])  # Placeholder model to set pipeline structure
+#     ])
+#
+#     # Grid search for hyperparameter tuning
+#     if param_grid:
+#         grid_search = GridSearchCV(
+#             pipeline, param_grid, cv=5, n_jobs=-1, scoring='accuracy')
+#         grid_search.fit(X, Y.ravel())
+#
+#         # Store the best model and score
+#         best_models[model_name] = {
+#             'best_estimator': grid_search.best_estimator_,
+#             'best_score': grid_search.best_score_
+#         }
+#         print(f"Best cross-validation accuracy for {model_name}: {grid_search.best_score_:.4f}")
+#     else:
+#         # Fit the pipeline without grid search if no parameters
+#         pipeline.fit(X, Y.ravel())
+#         best_models[model_name] = {
+#             'best_estimator': pipeline,
+#             'best_score': pipeline.score(X, Y.ravel())  # Evaluate the score directly
+#         }
+#         print(f"No hyperparameters to tune for {model_name}. Model fitted directly.")
+#
+# # Sort the models by their best score in descending order
+# sorted_models = sorted(best_models.items(), key=lambda item: item[1]['best_score'], reverse=True)
+#
+# # Select the top two models
+# best_two_models = sorted_models[:2]
+#
+# estimators = [(model_name, info['best_estimator']) for model_name, info in best_two_models]
+#
+# # Create a VotingClassifier with the best two models
+# voting_clf = VotingClassifier(estimators=estimators, voting='soft')  # You can also use 'hard' voting
+# voting_clf.fit(X, Y.ravel())
+#
+# try:
+#     joblib.dump(voting_clf, f"{path}/model.pkl")
+#     print(f'VotingClassifier saved successfully to {path}')
+# except Exception as e:
+#     print(f'Error saving the model: {e}')
