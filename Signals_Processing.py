c7f7485ff1df80cab2eecfb1bb09cad6fd344761
rodrigolima6 <rodrigoolivallima6@gmail.com>
2024-11-07 16:15:12 +0000

    fix(Training Model): move GridSearchCV to function in Signals_Processing.py.

--- Signals_Processing.py
+++ Signals_Processing.py
@@ -1,5 +1,12 @@
 import pyxdf
 from colorama import Fore
+from sklearn import neighbors, svm, naive_bayes, ensemble, feature_selection
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 
 from Epochs import *
@@ -176,7 +183,7 @@ def getFeatures(epochs_data: dict, fs: int, resolution: int) -> dict:
     for condition in tqdm(epochs_data):
         features[condition] = {}
         for epoch in tqdm(epochs_data[condition]["Signals"],
-                          desc=Fore.BLUE + f"Extracting feature for {condition}" + Fore.BLUE,
+                          desc=Fore.BLUE + f"Extracting features for {condition}" + Fore.BLUE,
                           colour="cyan",
                           bar_format=BLUE + "{l_bar}" + BLUE + "{bar}" + BLUE + "| " + BLUE + "{n_fmt}" + BLUE + "/" + BLUE + "{total_fmt} [" + BLUE + "{elapsed}" + "<" + BLUE + "{remaining}" + ", " + "{rate_fmt}""]",
                           leave=False):
@@ -227,6 +234,90 @@ def getFullDataframe(dataframe: pd.DataFrame, df_baseline: pd.DataFrame, columns
     full_dataframe["Condition"] = dataframe["Condition"]
 
     return full_dataframe
+
+
+@staticmethod
+def gridSearchCV(X: np.array, Y: np.array) -> dict:
+    """Models Parameters for GridSearch"""
+
+    param_grids = {
+        "KNN": {
+            'model': [neighbors.KNeighborsClassifier()],
+            'model__n_neighbors': list(range(1, 5)),
+            'model__weights': ['uniform', 'distance'],
+            'model__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+        },
+        "SVM": {
+            'model': [svm.SVC(probability=True)],
+            'model__C': [0.1, 1, 10],
+            'model__kernel': ['linear', 'rbf'],
+            'model__gamma': ["scale", "auto"],
+            'model__random_state': list(range(0, 50, 2))
+        },
+        "Bayes": {
+            'model': [naive_bayes.GaussianNB()]
+        },  # No parameters for GaussianNB
+
+        "Logistic Regression": {
+            'model': [LogisticRegression()],
+            'model__C': [0.1, 1, 10],
+            'model__solver': ['lbfgs', 'newton-cg', 'sag', 'saga'],
+            'model__multi_class': ["multinomial"],
+            'model__random_state': list(range(0, 50, 5))
+        },
+        "Random Forest": {
+            'model': [ensemble.RandomForestClassifier()],
+            'model__n_estimators': [50, 100, 150],
+            'model__criterion': ["gini", "entropy", "log_loss"],
+            'model__random_state': list(range(0, 50, 5))
+        },
+        "NN": {
+            'model': [MLPClassifier()],
+            'model__activation': ['identity', 'logistic', 'tanh', 'relu'],
+            'model__solver': ['lbfgs', 'sgd', 'adam'],
+            'model__random_state': list(range(0, 50, 5))
+        }
+    }
+
+    """Train Model using GridSearchCV"""
+
+    best_models = {}
+
+    # Loop through each model and parameter grid
+    for model_name, param_grid in tqdm(param_grids.items()):
+        # Create the pipeline
+        pipeline = Pipeline([
+            ('imputer', SimpleImputer(missing_values=np.nan, strategy="mean")),
+            ('scaler', StandardScaler()),
+            ('feature_selection', feature_selection.RFE(ensemble.RandomForestClassifier(random_state=42), step=1)),
+            ('model', param_grid['model'][0])  # Placeholder model to set pipeline structure
+        ])
+
+        # Grid search for hyperparameter tuning
+        if param_grid:
+            with tqdm(total=len(param_grid), desc=f"Hyperparameter tuning for {model_name}", leave=False) as inner_bar:
+                grid_search = GridSearchCV(
+                    pipeline, param_grid, cv=5, n_jobs=4, scoring='accuracy')
+                grid_search.fit(X, Y.ravel())
+
+                inner_bar.update(len(param_grid))
+
+                # Store the best model and score
+                best_models[model_name] = {
+                    'best_estimator': grid_search.best_estimator_,
+                    'best_score': grid_search.best_score_
+                }
+                print(f"Best cross-validation accuracy for {model_name}: {grid_search.best_score_ * 100:.2f}%")
+        else:
+            # Fit the pipeline without grid search if no parameters
+            pipeline.fit(X, Y.ravel())
+            best_models[model_name] = {
+                'best_estimator': pipeline,
+                'best_score': pipeline.score(X, Y.ravel())  # Evaluate the score directly
+            }
+            print(f"No hyperparameters to tune for {model_name}. Model fitted directly.")
+
+    return best_models
 
 # @staticmethod
 # def getVideosDict(epochs: dict, videos: dict) -> dict:
--- Training Model.py
+++ Training Model.py
@@ -83,85 +83,16 @@ try:
 except Exception as e:
     print(f"An error occurred saving the Full Dataframe: {e}")
 
-# """Input Data for Models"""
-# X = np.array(dataframe[columns])
-# Y = np.array(dataframe[["Category"]])
-#
-# """Models Parameters for GridSearch"""
-#
-# param_grids = {
-#     "KNN": {
-#         'model': [neighbors.KNeighborsClassifier()],
-#         'model__n_neighbors': list(range(1, 5)),
-#         'model__weights': ['uniform', 'distance'],
-#         'model__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
-#     },
-#     "SVM": {
-#         'model': [svm.SVC(probability=True)],
-#         'model__C': [0.1, 1, 10],
-#         'model__kernel': ['linear', 'rbf'],
-#         'model__gamma': ["scale", "auto"],
-#         'model__random_state': list(range(0, 50, 2))
-#     },
-#     "Bayes": {
-#         'model': [naive_bayes.GaussianNB()]
-#     },  # No parameters for GaussianNB
-#
-#     "Logistic Regression": {
-#         'model': [LogisticRegression()],
-#         'model__C': [0.1, 1, 10],
-#         'model__solver': ['lbfgs', 'newton-cg', 'sag', 'saga'],
-#         'model__multi_class': ["multinomial"],
-#         'model__random_state': list(range(0, 50, 5))
-#     },
-#     "Random Forest": {
-#         'model': [ensemble.RandomForestClassifier()],
-#         'model__n_estimators': [50, 100, 150],
-#         'model__criterion': ["gini", "entropy", "log_loss"],
-#         'model__random_state': list(range(0, 50, 5))
-#     },
-#     "NN": {
-#         'model': [MLPClassifier()],
-#         'model__activation': ['identity', 'logistic', 'tanh', 'relu'],
-#         'model__solver': ['lbfgs', 'sgd', 'adam'],
-#         'model__random_state': list(range(0, 50, 5))
-#     }
-# }
-#
-# """Train Model using GridSearchCV"""
-#
-# best_models = {}
-#
-# # Loop through each model and parameter grid
-# for model_name, param_grid in param_grids.items():
-#     # Create the pipeline
-#     pipeline = Pipeline([
-#         ('imputer', SimpleImputer(missing_values=np.nan, strategy="mean")),
-#         ('scaler', StandardScaler()),
-#         ('feature_selection', feature_selection.RFE(ensemble.RandomForestClassifier(random_state=42), step=1)),
-#         ('model', param_grid['model'][0])  # Placeholder model to set pipeline structure
-#     ])
-#
-#     # Grid search for hyperparameter tuning
-#     if param_grid:
-#         grid_search = GridSearchCV(
-#             pipeline, param_grid, cv=5, n_jobs=-1, scoring='accuracy')
-#         grid_search.fit(X, Y.ravel())
-#
-#         # Store the best model and score
-#         best_models[model_name] = {
-#             'best_estimator': grid_search.best_estimator_,
-#             'best_score': grid_search.best_score_
-#         }
-#         print(f"Best cross-validation accuracy for {model_name}: {grid_search.best_score_:.4f}")
-#     else:
-#         # Fit the pipeline without grid search if no parameters
-#         pipeline.fit(X, Y.ravel())
-#         best_models[model_name] = {
-#             'best_estimator': pipeline,
-#             'best_score': pipeline.score(X, Y.ravel())  # Evaluate the score directly
-#         }
-#         print(f"No hyperparameters to tune for {model_name}. Model fitted directly.")
+# full_dataframe = pd.read_csv(path + "\\full_dataframe.csv", sep=";")
+# columns = full_dataframe.columns[:(len(full_dataframe.columns) - 3)]
+
+"""Input Data for Models"""
+X = np.array(full_dataframe[columns])
+Y = np.array(full_dataframe[["Arousal"]])
+
+"""GridSearchCV"""
+best_models = gridSearchCV(X, Y)
+
 #
 # # Sort the models by their best score in descending order
 # sorted_models = sorted(best_models.items(), key=lambda item: item[1]['best_score'], reverse=True)
